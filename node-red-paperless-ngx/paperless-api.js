4e8dd3e0392940d795f6983ee4ae71110a9c6bb6
Martin Möllenbeck <martin.moellenbeck@5minds.de>
2025-01-03 22:53:59 +0100

    Add logging

--- node-red-paperless-ngx/paperless-api.js
+++ node-red-paperless-ngx/paperless-api.js
@@ -19,6 +19,8 @@ module.exports = class PaperlessNgxApi {
             url += `?${params}`
         }
 
+        console.log(url)
+
         const request = {
             method: method,
             credentials: 'include',
